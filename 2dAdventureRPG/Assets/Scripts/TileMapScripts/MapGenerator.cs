c5349dc37c253510a9ecdb779e8b957a7fdc68bc
UtkarshJha329 <utkarshjha329@gmail.com>
2025-03-28 00:46:01 +0530

    Simple Room Partitioning.
    Room is partitioned into smaller axis aligned sections that do not intersect with each other.
    
    TODO: Generate pathways in between the sections and populate area with enemies.

--- 2dAdventureRPG/Assets/Scripts/TileMapScripts/MapGenerator.cs
+++ 2dAdventureRPG/Assets/Scripts/TileMapScripts/MapGenerator.cs
@@ -1,6 +1,15 @@
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+
+public enum TileBorderType
+{
+    TOP_MIDDLE,
+    BOTTOM_MIDDLE,
+    RIGHT_MIDDLE,
+    LEFT_MIDDLE
+}
 
 public class MapGenerator : MonoBehaviour
 {
@@ -44,7 +53,14 @@ public class MapGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            FillRoomWithMaze(new Vector2Int(2 * numTilesInRooms.x, 1 * numTilesInRooms.y));
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            FillRoomWithMaze(new Vector2Int(2 * numTilesInRooms.x, 1 * numTilesInRooms.y), true);
+        }
     }
 
     private void GenerateMap()
@@ -100,9 +116,10 @@ public class MapGenerator : MonoBehaviour
 
     }
 
-    private void FillRoomWithMaze(Vector2Int offsetInTiles)
+    private void FillRoomWithMaze(Vector2Int offsetInTiles, bool rightBorders = false, bool fixTiles = false)
     {
         bool[,] marked = new bool[numTilesInRooms.x, numTilesInRooms.y];
+        byte[,] tileType = new byte[numTilesInRooms.x, numTilesInRooms.y];
 
         for (int y = 0; y < numTilesInRooms.y; y++)
         {
@@ -110,62 +127,115 @@ public class MapGenerator : MonoBehaviour
             {
                 if (!marked[x, y])
                 {
-                    int randomSizeX = Random.Range(3, numTilesInRooms.x - x);
-                    int randomSizeY = Random.Range(3, numTilesInRooms.y - y);
+                    int maxWidth = numTilesInRooms.x - x;
+                    int maxHeight = numTilesInRooms.y - y;
+
+                    for (int i = 0; i < maxWidth; i++)
+                    {
+                        if (marked[i + x, y])
+                        {
+                            //Debug.Log("Extended Max Width from : " + maxWidth + ", to : " + i);
+                            maxWidth = i + 1;
+                            break;
+                        }
+                    }
+
+                    int randomSizeX = Random.Range(3, maxWidth);
+                    int randomSizeY = Random.Range(3, maxHeight);
                     Vector2Int randomSectionSize = new Vector2Int(randomSizeX, randomSizeY);
 
-                    int remainingTilesX = numTilesInRooms.x - x - randomSizeX;
+                    //int remainingTilesX = numTilesInRooms.x - x - randomSectionSize.x;
+                    int remainingTilesX = maxWidth - randomSectionSize.x;
                     Vector2Int added = new Vector2Int(0, 0);
                     if (remainingTilesX < 4)
                     {
                         added.x = remainingTilesX;
                         randomSectionSize.x += remainingTilesX;
-                    }
+                    }                    
 
-                    int remainingTilesY = numTilesInRooms.y - y - randomSizeY;
+                    int remainingTilesY = numTilesInRooms.y - y - randomSectionSize.y;
                     if (remainingTilesY < 4)
                     {
                         added.y = remainingTilesY;
                         randomSectionSize.y += remainingTilesY;
                     }
 
-                    Debug.Log("Random Section Size : " + randomSectionSize.x + ", " + randomSectionSize.y + " From : " + x + ", " + y);
-                    Debug.Log("Added " + added.x + ", " + added.y);
+                    //Debug.Log("Random Section Size : " + randomSectionSize.x + ", " + randomSectionSize.y + " From : " + x + ", " + y);
+                    //Debug.Log("Added " + added.x + ", " + added.y);
 
                     for (int j = y; j < (y + randomSectionSize.y); j++)
                     {
                         for (int i = x; i < (x + randomSectionSize.x); i++)
                         {
-                            //Debug.Log("Before : " + i + ", " + j);
-                            marked[i, j] = true;
-                            //Debug.Log("After : " + i + ", " + j);
+                            if (!marked[i, j])
+                            {
+                                //Debug.Log("Before : " + i + ", " + j);
+                                marked[i, j] = true;
+                                //Debug.Log("After : " + i + ", " + j);
 
-                            if (j == y)
-                            {
-                                //Bottom border of the section.
-                                groundTileMap.SetTile(new Vector3Int(offsetInTiles.x + i, offsetInTiles.y + j, 0), groundTileGrassBM);
-                            }
-                            else if (j == (y + randomSectionSize.y - 1))
-                            {
-                                //Top border of the section.
-                                groundTileMap.SetTile(new Vector3Int(offsetInTiles.x + i, offsetInTiles.y + j, 0), groundTileGrassTM);
-                            }
-                            else if (i == x)
-                            {
-                                //Left border of the section.
-                                groundTileMap.SetTile(new Vector3Int(offsetInTiles.x + i, offsetInTiles.y + j, 0), groundTileGrassML);
-                            }
-                            else if (i == (x + randomSectionSize.x - 1))
-                            {
-                                //Right border of the section.
-                                groundTileMap.SetTile(new Vector3Int(offsetInTiles.x + i, offsetInTiles.y + j, 0), groundTileGrassMR);
-                            }
-                            else
-                            {
-                                //Non border of the section.
-                                groundTileMap.SetTile(new Vector3Int(offsetInTiles.x + i, offsetInTiles.y + j, 0), groundTileGrassMM);
+                                Vector3Int curTilePosInTileMapGrid = new Vector3Int(offsetInTiles.x + i, offsetInTiles.y + j, 0);
+                                bool borderTile = false;
+                                if (j == y)
+                                {
+                                    //Bottom border of the section.
+                                    groundTileMap.SetTile(curTilePosInTileMapGrid, groundTileGrassBM);
+                                    //groundTileMap.SetTile(curTilePosInTileMapGrid, groundTileGrassMM);
+                                    borderTile = true;
+                                    tileType[i, j] = (byte)TileBorderType.BOTTOM_MIDDLE;
+                                }
+                                else if (j == (y + randomSectionSize.y - 1))
+                                {
+                                    //Top border of the section.
+                                    //groundTileMap.SetTile(curTilePosInTileMapGrid, groundTileGrassTM);
+                                    //groundTileMap.SetTile(curTilePosInTileMapGrid, groundTileGrassMM);
+                                    //groundTileMap.SetTile(curTilePosInTileMapGrid, groundTileDrySand);
+                                    marked[i, j] = false;
+                                    borderTile = true;
+                                    tileType[i, j] = (byte)TileBorderType.TOP_MIDDLE;
+                                }
+
+                                if (i == x)
+                                {
+                                    //Left border of the section.
+                                    groundTileMap.SetTile(curTilePosInTileMapGrid, groundTileGrassML);
+                                    //groundTileMap.SetTile(curTilePosInTileMapGrid, groundTileGrassMM);
+                                    borderTile = true;
+                                    tileType[i, j] = (byte)TileBorderType.LEFT_MIDDLE;
+                                }
+                                else if (i == (x + randomSectionSize.x - 1))
+                                {
+                                    //Right border of the section.
+                                    //groundTileMap.SetTile(curTilePosInTileMapGrid, groundTileGrassMR);
+                                    //groundTileMap.SetTile(curTilePosInTileMapGrid, groundTileGrassMM);
+                                    //groundTileMap.SetTile(curTilePosInTileMapGrid, groundTileDrySand);
+                                    //marked[i, j] = false;
+                                    marked[i, j] = rightBorders;
+                                    borderTile = true;
+                                    tileType[i, j] = (byte)TileBorderType.RIGHT_MIDDLE;
+                                }
+
+                                if (!borderTile)
+                                {
+                                    //Non border of the section.
+                                    //groundTileMap.SetTile(curTilePosInTileMapGrid, groundTileGrassMM);
+                                    groundTileMap.SetTile(curTilePosInTileMapGrid, groundTileDrySand);
+                                }
                             }
                         }
+                    }
+                }
+            }
+        }
+
+        if (fixTiles)
+        {
+            for (int y = 1; y < numTilesInRooms.y; y++)
+            {
+                for (int x = 0; x < numTilesInRooms.x; x++)
+                {
+                    if (tileType[x, y] == (byte)TileBorderType.BOTTOM_MIDDLE && tileType[x, y - 1] == (byte)TileBorderType.BOTTOM_MIDDLE)
+                    {
+                        groundTileMap.SetTile(new Vector3Int(offsetInTiles.x + x, offsetInTiles.y + y - 1, 0), groundTileDrySand);
                     }
                 }
             }
